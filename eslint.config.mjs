4f05481d7cb4e95560165397a819d172887f9a65
patdel0 <zefiru@protonmail.com>
2025-03-26 10:05:51 -0300

    Set up TS and Eslint for Airbnb compatibility
    Disabled the rule which requires the "React" import, as it is no longer
    reequired in newer versions of React.
    
    Example error:
    ```
    /home/dev/code/github.com/100devs-tobenamed/src/app/page.tsx
      3:5  error  'React' must be in scope when using JSX  react/react-in-jsx-scope
    ```

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tsEslintParser from '@typescript-eslint/parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { ignores: ['.next/**/*'] },
+  ...compat.extends('next/core-web-vitals', 'next', 'airbnb', 'airbnb-typescript'),
+  {
+    languageOptions: {
+      parser: tsEslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
