9fd75748ba28e9dda45e18b8d35992fab5dc3729
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-05-05 14:52:15 +0100

    added baseplate for navbar, waiting on design choices

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+// This is the navbar component that will be used in the app
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-col'>
+       <ul>
+        <li> <Link href="/home">Home</Link></li>
+        <li> <Link href="/profile">Profile</Link></li>
+        <li> <Link href="/quests">Quests</Link></li>
+       </ul>
+    </nav>
+  )
+}
+
+export default Navbar
